1fee83f9fbb5d0b801b3103c4247639b423cf441
xkrejc70 <jankrejci77@gmail.com>
2025-01-04 16:37:18 +0100

    Docker compose

--- app/public/index.php
+++ app/public/index.php
@@ -1,9 +1,14 @@
 <?php
+
+use Dotenv\Dotenv;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
 $app = AppFactory::create();
 $app->addRoutingMiddleware();
